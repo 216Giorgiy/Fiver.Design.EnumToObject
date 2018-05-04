a8c0af0be3b713dfbbbae425f4ee00ba6d78283a
Tahir Naushasd <t.naushad25@gmail.com>
2018-05-04 23:01:33 +0100

    Use Objects instead of Enums in Domain Logic for better design

---
+++ Fiver.Design.EnumToObject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fiver.Design.EnumToObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
